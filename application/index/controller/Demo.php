a5be8560013d46c811450319532de54bc976280e
Lee <605333742@qq.com>
2018-12-06 22:40:11 +0800

    优化微博登陆-Lee

--- application/index/controller/Demo.php
+++ application/index/controller/Demo.php
@@ -1,21 +1,24 @@
 <?php
 /**
  * 测试文件
- * 
+ *
  * @author Lee<a605333742@gmail.com>
  * @date 2018-08-28
  */
 namespace app\index\controller;
 
-class Demo{
+use ext\sina\SaeTClientV2;
+use ext\sina\SaeTOAuthV2;
+use think\Controller;
+
+class Demo extends Controller{
     /*
      * 默认控制器
-     * 
+     *
      * @return #
      */
-    public function index($id='',$name=''){
-        var_dump($id);
-        var_dump($name);
-        echo "这里是  　　index/Demo/index";exit;
+    public function index(){
+	$model=new SaeTClientV2();
+	var_dump($model);exit;
     }
 }
