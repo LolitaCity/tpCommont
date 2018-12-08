3e5dc49ed142e5984adb4d03e488621ba15eb778
Lee <605333742@qq.com>
2018-12-08 12:55:34 +0800

    优化phpinfo显示-lee

---
+++ application/index/controller/PhpInfo.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * phpinfo 显示
+ * 
+ * @author Lee<a605333742@gmail.com>
+ * @date    2018-12-08
+ */
+
+namespace app\index\controller;
+
+use think\Controller;
+
+class PhpInfo extends Controller{
+    /**
+     * 构造函数
+     * 
+     * @return #
+     */
+    public function __construct(){
+        parent::__construct();
+    }
+    
+    /**
+     * 显示PHPinfo信息
+     * 
+     * @return #
+     */
+    public function index(){
+        phpinfo();exit;
+    }
+}
